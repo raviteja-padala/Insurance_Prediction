d8378ca8f882a501cd63d8cbc797f04ecba8cea8
Lenovo <https://github.com/raviteja-padala/git-push.git>
2023-07-12 22:14:24 +0530

    Till model evaluation

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ import os , sys
 from Insurance.utils import get_collection_as_dataframe
 from Insurance.entity.config_entity import DataIngestionConfig
 from Insurance.entity import config_entity
+from Insurance.components.data_ingestion import DataIngestion
+from Insurance.components.data_validation import DataValidation 
+from Insurance.components.data_transformation import DataTransformation
+from Insurance.components.model_trainer import ModelTrainer
+from Insurance.components.model_evaluation import ModelEvaluation
 
 #def test_logger_and_exception():
     #try:
@@ -22,5 +27,42 @@ if __name__ == "__main__":
         training_pipeline_config = config_entity.TrainingPipelineConfig()
         data_ingestion_config = config_entity.DataIngestionConfig(training_pipeline_config= training_pipeline_config)
         print(data_ingestion_config.to_dict())
+
+        data_ingestion = DataIngestion(data_ingestion_config= data_ingestion_config)
+        data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+        print("Data Ingestion done")
+
+        #data validation
+        data_validation_config = config_entity.DataValidationConfig(training_pipeline_config=training_pipeline_config)
+        data_validation = DataValidation(data_validation_config=data_validation_config, data_ingestion_artifact=data_ingestion_artifact )
+
+        data_validation_artifact = data_validation.initiate_data_validation()
+        print("Data Validation Done")
+
+        #Data transformation
+        data_transformation_config = config_entity.DataTransformationConfig(training_pipeline_config= training_pipeline_config)
+        data_transformation = DataTransformation(data_transformation_config=data_transformation_config, data_ingestion_artifact=data_ingestion_artifact)
+
+        data_transformation_artifact=data_transformation.initiate_data_transformation()
+        print("Data Transformation Done")
+
+        #Model Training
+        model_trainer_config=config_entity.ModelTrainerConfig(training_pipeline_config=training_pipeline_config)
+        model_trainer= ModelTrainer(model_trainer_config=model_trainer_config, data_transformation_artifact=data_transformation_artifact)
+
+        model_trainer_artifact = model_trainer.initiate_model_trainer()
+        print("Model Training Done")
+
+
+        #model evaluation
+        model_eval_config = config_entity.ModelEvaluationConfig(training_pipeline_config=training_pipeline_config)
+        model_eval  = ModelEvaluation(model_eval_config=model_eval_config,
+        data_ingestion_artifact=data_ingestion_artifact,
+        data_transformation_artifact=data_transformation_artifact,
+        model_trainer_artifact=model_trainer_artifact)
+        model_eval_artifact = model_eval.initiate_model_evaluation()
+        print("Model Evaluation Done")
+        
+
     except Exception as e:
         print(e)
